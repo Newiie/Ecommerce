e341c22582fefa8976ccf8dd45ac0012fa31ecbb
JethroCenas <87797179+Newiie@users.noreply.github.com>
2024-08-30 21:46:58 +0800

    sample

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Login from './pages/Login.jsx'
